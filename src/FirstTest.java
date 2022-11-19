73ffd95c2cdd6c200afaa9070270ea99524bde76
rubayatsaikat <saikat091992@gmail.com>
2022-11-19 13:19:39 +0600

    first test

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,12 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Run Successfully");
-		System.out.println("All Okay");
-		System.out.println("Congrats");
+		
+		// Setting the Chrome driver property
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\geckodriver.exe");
+		// Instantiate Chrome driver
+		WebDriver driver = new ChromeDriver();
+		//WebDriver driver2 = new FirefoxDriver();
+		// Opening Google HomePage in Chrome Browser
+		driver.get("https://www.google.com");
+		//driver2.get("https://www.bing.com");
+		//Quitting the driver
+		//driver.quit();
 	}
 
 }
